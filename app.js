318681362e916e48cc38abcd6b0ee22e21c2d316
Szymon <szymon.osinski.2009@gmail.com>
2023-02-28 21:23:38 +0100

    rebuilding the system Calculator

--- app.js
+++ app.js
@@ -3,14 +3,17 @@ const operatorsButtons = document.querySelectorAll('.operator');
 const equalsButton = document.querySelector('.equals');
 const clearButton = document.querySelector('.clear');
 const input = document.querySelector('.input')
-const parent = document.querySelector('.historyParent')
+const parent = document.querySelector(".historyParent")
 const lastExquation = document.querySelector('.lastExquation')
+const canel = document.querySelector('.canel')
 let valueExquation = '';
 let value = '';
 let exquation = value;
 let isResult = false;
 let result = eval(value);
 
+
+// popraw zapisz instrukcji warunkowej dotyczącej decimalPart
 function displayNumbers () {
     if(this.textContent === '.' && value[value.length -1] === '.'){
         return;
@@ -18,60 +21,84 @@ function displayNumbers () {
     if(this.textContent === '.' && value === ''){
         return 
     }
-    value = value + this.textContent;
-    input.value = value
+    input.value = input.value + this.textContent
     spanfunction()
-    isResult = false;
 }
 
 function operate () {
-    if(value[value.length -1] === '*' ||value[value.length -1] === '-' || value[value.length -1] === '+' || value[value.length -1] === '/' ){
+    if(input.value[input.value.length -1] === '*' ||
+       input.value[input.value.length -1] === '-' ||
+       input.value[input.value.length -1] === '+' || 
+       input.value[input.value.length -1] === '/' || 
+       input.value === ''){
         return;
     }
-    if(value === '' && this.textContent === '-'){
-        value = value + this.textContent;
-        input.value = value
-        return;
-    }else if(value === '' || this.textContent === '%'){
-        return;
-    }
-    value = value + this.textContent
-    input.value = value
+    input.value = input.value + this.textContent
     spanfunction()
-    isResult = false;
 }
 
+canel.addEventListener('click', () => {
+    input.value = input.value.slice(0, -1);
+    spanfunction()
+})
+
 function showResult() {
-    if(value === ''){
-        return;
+    const regex = /[*+-/]/;
+    lastExquation.innerHTML = ""
+    exquation = input.value
+    if(regex.test(input.value)){
+        try{
+            result = eval(input.value)
+            result = result.toString()
+            result = result.slice(0, 14)
+        }catch (erroe){
+            return;
+        }
+        input.value = result
+        lastExquation.innerHTML = exquation + ' ='
+        addToHistory()
     }
-    exquation = value;
-    result = eval(value);
-    result = Number(result.toFixed(14));
-    input.value = result;
-    value = Number(result.toFixed(2)).toString();
-    addToHistory();
-    spanfunction();
-    isResult = true;
 }
 
 function spanfunction() {
-    if(isResult === false){
-        
+    const regex = /[*+-/]/;
+    lastExquation.innerHTML = ""
+    if(regex.test(input.value)){
+        try{
+            result = eval(input.value)
+        }catch (erroe){
+            return;
+        }
+        let decimalPart = result.toString().split(".")[1];
+        if(decimalPart === 1){
+            result = result.toFixed(1)
+        }else if(decimalPart >= 2){
+            result = result.toFixed(2)
+        }
+        lastExquation.innerHTML = result
     }else{
+        return;
     }
 }
 
 function clear() {
     value = '';
     input.value = value;
+    lastExquation.innerHTML = ''
 }
 
 function addToHistory(){
-    const newExquation = document.createElement("li");
+    const historyResult = Number(result)
+    let decimalPart = historyResult.toString().split(".")[1];
+    if(decimalPart === 1){
+        historyResult = historyResult.toFixed(1)
+    }else if(decimalPart >= 2){
+        historyResult = historyResult.toFixed(2)
+    }
+    const newExquation = document.createElement('li')
     newExquation.className = 'child'
     newExquation.innerHTML = `
-    ${exquation} = ${value}
+    ${exquation} = ${historyResult.toFixed(2)}
     `
     parent.appendChild(newExquation);
 }
@@ -89,10 +116,14 @@ operatorsButtons.forEach((button) => {
 equalsButton.addEventListener('click', showResult)
 
 
+input.addEventListener('input', spanfunction)
+
 input.addEventListener('keypress', (event) =>{
     let regex = /[!@#$%^&*()_=[\]{};':"\\ |,.<>\?`~]/g;
     let key = String.fromCharCode(event.keyCode);
     if((event.keyCode >= 65 && event.keyCode <= 90) || (event.keyCode >= 97 && event.keyCode <= 122) || regex.test(key)) {
       event.preventDefault();
+    }else if(event.keyCode === 13){
+        showResult()
     }
 });
